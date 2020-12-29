6d2ece9588d4275f7f8f5d03335ab389ef4ee747
Mario Bove <mariobove3@gmail.com>
2020-12-29 18:45:49 +0100

    Zuul Api gateway
    Zuul Api gateway

--- microservizi/AuthenticationMS/src/main/java/com/gruppo13/AuthenticationMS/controller/AuthController.java
+++ microservizi/AuthenticationMS/src/main/java/com/gruppo13/AuthenticationMS/controller/AuthController.java
@@ -14,10 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import lombok.extern.slf4j.Slf4j;
@@ -35,6 +32,11 @@ public class AuthController {
 
     @Autowired
     TokenProvider tokenProvider;
+
+    @PostMapping("/figlioDiP")
+    public ResponseEntity<?> figliodip(){
+        return ResponseEntity.ok("new JwtAuthenticationResponse(jwt, GeneralUtils.buildUserInfo(localUser))");
+    }
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
---
+++ microservizi/ZuulApiGateway/src/main/java/com/example/ZuulApiGateway/configuration/GatewayConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.ZuulApiGateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class GatewayConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authentication/**")
+                .permitAll()
+                .antMatchers("/**")
+                .permitAll();
+    }
+}
