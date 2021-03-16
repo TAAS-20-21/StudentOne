55e29ef532b94f2da85d674e1e8f2f6ec1e908ea
michele <lanotte28@gmail.com>
2021-03-16 16:41:43 +0100

    fix bug

--- Front-End/Web App/src/app/common/app.constants.ts
+++ Front-End/Web App/src/app/common/app.constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
   private static API_BASE_URL = "http://localhost:8080/StudentOne/";
-  private static OAUTH2_URL = AppConstants.API_BASE_URL + "authenticationservice/oauth2/authorization/";
+  private static OAUTH2_URL = AppConstants.API_BASE_URL + "authenticateservice/oauth2/authorization/";
   private static REDIRECT_URL = "?redirect_uri=http://localhost:4200/login";
   public static API_URL = AppConstants.API_BASE_URL + "authenticationservice/api/";
   public static AUTH_API = AppConstants.API_URL + "auth/";
