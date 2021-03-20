e5b41b96d54158c33fa1890dc4fbb9806b61141b
Francesco Lomuscio <francilomuscio@gmail.com>
2021-03-20 01:05:47 +0100

    fix ip in android app

--- Front-End/Android App/app/src/main/java/com/example/studentoneapp/RetrofitClient.java
+++ Front-End/Android App/app/src/main/java/com/example/studentoneapp/RetrofitClient.java
@@ -14,8 +14,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private String AUTH ="";
-    public static  final String BASE_URL = "http://10.0.2.2:8080/StudentOne/";
-    public static final String AUTH_URL = BASE_URL + "authenticationservice/api/";
+    public static  final String BASE_URL = "http://192.168.1.68:8080/StudentOne/";
+    public static final String AUTH_URL = BASE_URL + "authenticateservice/api/";
     public static  final String COURSE_URL = "http://10.0.2.2:8080/StudentOne/courseservice/api/";
     private static com.example.studentoneapp.RetrofitClient mInstance;
     private Retrofit retrofit;
